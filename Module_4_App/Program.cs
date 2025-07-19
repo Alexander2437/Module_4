29020b0ce86babd60d89a3f1e55cbef43153b047
Alexander2437 <alexander.vtyurin@gmail.com>
2025-07-19 16:18:23 +0700

    Add project files.

---
+++ Module_4_App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Module_4_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
