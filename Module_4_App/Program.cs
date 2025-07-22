25dc836c086611f57220ddc8a40c03a194cea216
Alexander2437 <alexander.vtyurin@gmail.com>
2025-07-22 16:29:19 +0700

    feature: возможность остановить программу(break) или пропустить действие (continue)

--- Module_4_App/Program.cs
+++ Module_4_App/Program.cs
@@ -7,36 +7,41 @@ namespace Module_4_App
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Напишите свой любимый цвет на английском языке с маленькой буквы:");
-
-            var color = Console.ReadLine();
-
-            switch (color)
+            for (int i = 1; i < 5; i++)
             {
-                case "white":
-                    Console.BackgroundColor = ConsoleColor.White;
-                    Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("Iteration {0}", i);
 
-                    Console.WriteLine("Your colour is white!");
-                    break;
-                case "blue":
-                    Console.BackgroundColor = ConsoleColor.Blue;
-                    Console.ForegroundColor = ConsoleColor.Black;
+                var text = Console.ReadLine();
 
-                    Console.WriteLine("Your color is blue!");
+                if (text == "stop")
+                {
+                    Console.WriteLine("The cycle is stopped.");
                     break;
-                case "red":
-                    Console.BackgroundColor = ConsoleColor.Red;
-                    Console.ForegroundColor = ConsoleColor.Black;
+                }
 
-                    Console.WriteLine("Your colour is red!");
-                    break;
-                default:
-                    Console.BackgroundColor = ConsoleColor.Red;
-                    Console.ForegroundColor = ConsoleColor.Black;
+                switch (text)
+                {
+                    case "white":
+                        Console.BackgroundColor = ConsoleColor.White;
+                        Console.ForegroundColor = ConsoleColor.Black;
 
-                    Console.WriteLine("Wrong! Reconsider your oppinion! =)");
-                    break;
+                        Console.WriteLine("Your colour is white!");
+                        break;
+                    case "blue":
+                        Console.BackgroundColor = ConsoleColor.Blue;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is blue!");
+                        break;
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your colour is red!");
+                        break;
+                    default:
+                        continue;
+                }
             }
 
             Console.ReadKey();
