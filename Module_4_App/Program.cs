c87598deb924bd22ae030688f1f1bff2692a37e8
Alexander2437 <alexander.vtyurin@gmail.com>
2025-07-23 18:27:19 +0700

    Задание 4.3.13.1. Вывод суммы всех элементов массива.

--- Module_4_App/Program.cs
+++ Module_4_App/Program.cs
@@ -8,25 +8,21 @@ namespace Module_4_App
         static void Main(string[] args)
         {
             var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
-
-            int temp;
+            var sum = 0;
 
             for (int i = 0; i < arr.Length; i++)
             {
-                for (int j = i + 1; j < arr.Length; j++)
-                {
-                    if (arr[i] > arr[j])
-                    {
-                        temp = arr[i];
-                        arr[i] = arr[j];
-                        arr[j] = temp;
-                    }
-                }
+                sum += arr[i];
             }
-            foreach (var num in arr)
-            {
-                Console.Write(num + " ");
-            }
+
+            Console.WriteLine(sum);
+
+            Console.ReadKey();
+
+            //foreach (var item in arr)
+            //{
+            //    Console.Write(item + " ");
+            //}
         }
     }
 }
