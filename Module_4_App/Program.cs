643fd420b54cf5456504679dcbb895b6a0c6a475
Alexander2437 <alexander.vtyurin@gmail.com>
2025-07-26 18:14:40 +0700

    Задания 4.5.1. - 4.5. 6. Пользовательская анкета.

--- Module_4_App/Program.cs
+++ Module_4_App/Program.cs
@@ -4,19 +4,43 @@
     {
         static void Main(string[] args)
         {
-            (string Name, string Type, double Age, int NameCount) Pet;
+            (string firstName, string lastName, int age, string login, int loginLength, bool hasPet, string[] favColors) User;
 
-            Console.Write("Pet's name: ");
-            Pet.Name = Console.ReadLine();
-            Pet.NameCount = Pet.Name.Length;
+            for (int j = 0; j < 3; j++)
+            {
+                Console.Write("Insert user name: ");
+                User.firstName = Console.ReadLine();
 
-            Console.Write("Pet's type: ");
-            Pet.Type = Console.ReadLine();
+                Console.Write("Insert user surname:");
+                User.lastName = Console.ReadLine();
 
-            Console.Write("Pet's age: ");
-            Pet.Age = double.Parse(Console.ReadLine());
+                Console.Write("Insert password: ");
+                User.login = Console.ReadLine();
+                User.loginLength = User.login.Length;
 
-            Console.Write($"\nPet's name is {Pet.Name}.\nPet's type is {Pet.Type}.\nPet's age is {Pet.Age}.\nPet's name length is {Pet.NameCount}");
+                Console.Write("Do you have a pet? (Answer \"Yes\"\"No\".) ");
+                var result = Console.ReadLine();
+
+                if (result == "Yes")
+                {
+                    User.hasPet = true;
+                }
+                else
+                {
+                    User.hasPet = false;
+                }
+
+                Console.Write("Insert user age: ");
+                User.age = int.Parse(Console.ReadLine());
+
+                User.favColors = new string[3];
+                Console.WriteLine("Insert three favourite colours:");
+
+                for (int i = 0; i < User.favColors.Length; i++)
+                {
+                    User.favColors[i] = Console.ReadLine();
+                }
+            }
 
             Console.ReadKey();
         }
